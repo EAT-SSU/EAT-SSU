47ee985934e418e6cdfcc3bf0f3a59b9cdeb6a32
Yujin <jini1514@soongsil.ac.kr>
2022-11-16 14:14:22 +0900

    feat: 로그인, 회원가입 activity에 대한 id 값 부여 & 파이어베이스 연동 코드 작성 #6

--- app/src/main/java/com/example/eatssu/loginActivity.java
+++ app/src/main/java/com/example/eatssu/loginActivity.java
@@ -1,14 +1,75 @@
 package com.example.eatssu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class loginActivity extends AppCompatActivity {
+
+    Button mLoginBtn;
+    TextView mResigettxt;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mResigettxt = findViewById(R.id.btn_movetosignup); //맞나
+        mLoginBtn = findViewById(R.id.btn_login);
+        mEmailText = findViewById(R.id.et_EmailAddress);
+        mPasswordText = findViewById(R.id.et_Password);
+
+
+        //가입 버튼이 눌리면
+        mResigettxt.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(loginActivity.this,SignUpActivity.class)); //맞나
+
+            }
+        });
+
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(loginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    //Intent intent = new Intent(loginActivity.this, BottomNaviActivity.class);
+                                    //startActivity(intent);
+                                }else{
+                                    Toast.makeText(loginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
+
 }
