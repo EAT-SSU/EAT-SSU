ac62ef58a817d063fb1339c7dc2f1e51ae6b679f
Yujin <jini1514@soongsil.ac.kr>
2022-11-22 02:57:55 +0900

    feat: 보드 액티비티 수정 #15

--- app/src/main/java/com/example/eatssu/BoardFragment.java
+++ app/src/main/java/com/example/eatssu/BoardFragment.java
@@ -1,21 +1,49 @@
 package com.example.eatssu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
+import androidx.viewbinding.ViewBinding;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import com.example.eatssu.databinding.FragmentBoardBinding;
 
 public class BoardFragment extends Fragment {
+    private ViewDataBinding binding;
+    //Button goWriteButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View B=inflater.inflate(R.layout.fragment_board,container,false);
+        View root=inflater.inflate(R.layout.fragment_board,container,false);
 
-        return B;
+        //binding = DataBindingUtil.inflate(inflater,R.layout.fragment_board,container,false);
+        //View root = binding.getRoot();
+
+        /*
+        Button goWriteButton = (Button) getView().findViewById(R.id.btn_goWrite);
+
+        goWriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),WriteBoardActivity.class);
+                startActivity(intent);
+            }
+        });*/
+        return root;
     }
+
+//    final ArrayAdapter adapter = new ArrayAdapter(this,R.layout.content,R.id.tv_title);
+
+
 }
