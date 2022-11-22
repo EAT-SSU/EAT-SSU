594c2e20ecff9fc564f60436da1cfabe98a74dfc
binisnull <dbsqlssss0704@gmail.com>
2022-11-22 14:34:26 +0900

    Feat : 리뷰화면 3 -> 리뷰화면 1 전환 구현 #7

--- app/src/main/java/com/example/eatssu/ReviewActivity3.java
+++ app/src/main/java/com/example/eatssu/ReviewActivity3.java
@@ -2,7 +2,10 @@ package com.example.eatssu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReviewActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ReviewActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review3);
+        Button buttonNext = findViewById(R.id.registReviewBtn);
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ReviewActivity3.this,ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
