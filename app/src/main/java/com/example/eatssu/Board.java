4febd4a8950ba04c8fdc484142a292cd0a5bdcaf
Yujin <jini1514@soongsil.ac.kr>
2022-11-26 01:48:29 +0900

    feat: 리사이클러뷰의 어뎁터 코드 작성 및 파이어베이스 realtime db 연결 #15

--- app/src/main/java/com/example/eatssu/Board.java
+++ app/src/main/java/com/example/eatssu/Board.java
@@ -3,21 +3,70 @@ package com.example.eatssu;
 //제목, 본문 내용, 시간, 아이디, 좋아요 개수, 메시지 개수
 
 class Board {
-    String title;
-    String content;
-    int id;
-    int likeCount;
-    int messageCount;
-    int datetime;
-
+    private String title;
+    private String content;
+    private int id;
+    private int likeCount;
+    private int messageCount;
+    private int datetime;
+//alt insert
 
     Board(){}
 
-    Board(String title, String content){
-        this.title=title;
-        this.content=content;
+    public String getTitle() {
+        return title;
     }
 
-    public String getTitle(){return title;}
-    public String getContent(){return content;}
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public int getMessageCount() {
+        return messageCount;
+    }
+
+    public void setMessageCount(int messageCount) {
+        this.messageCount = messageCount;
+    }
+
+    public int getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(int datetime) {
+        this.datetime = datetime;
+    }
+
+    //
+//    Board(String title, String content){
+//        this.title=title;
+//        this.content=content;
+//    }
+//
+//    public String getTitle(){return title;}
+//    public String getContent(){return content;}
 }
--- app/src/main/java/com/example/eatssu/BoardFragment.java
+++ app/src/main/java/com/example/eatssu/BoardFragment.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +18,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.example.eatssu.databinding.FragmentBoardBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class BoardFragment extends Fragment {
-    private ViewDataBinding binding;
+    //private viewBinding binding;
     //Button goWriteButton;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Board> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Nullable
     @Override
@@ -29,6 +43,38 @@ public class BoardFragment extends Fragment {
 
         //binding = DataBindingUtil.inflate(inflater,R.layout.fragment_board,container,false);
         //View root = binding.getRoot();
+
+
+        recyclerView=view.findViewById(R.id.RV);
+        recyclerView.setHasFixedSize(true); //리사이클러뷰 기존 성능 강화
+        //layoutManager = new LinearLayoutManager(this); //아 프레그먼트에서 디스 안되는데
+        arrayList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Board");//DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); //기존 배열리스트가 존재하지 않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Board board = snapshot.getValue(Board.class); //만들어뒀던 Board 객체에 데이터를 담는다.
+                    arrayList.add(board); //실제로 추가가 됨. 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); //리스트 저장 및 새로 고침
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //디비를 가져오던 중 에러 발생시
+                //Log.e("BoardFragment",String.valueOf(databaseError.toException()));//에러문 출력
+                //안해도 됨
+            }
+        });
+
+        adapter = new CustomAdapter(arrayList,this);
+        recyclerView.setAdapter(adapter);
 
         Button goWriteButton = (Button) view.findViewById(R.id.btn_goWrite);
 
