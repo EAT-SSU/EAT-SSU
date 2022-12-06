45e75a9588ee960758d707e816c42a31ba765d94
Yujin <jini1514@soongsil.ac.kr>
2022-12-06 12:53:30 +0900

    add: Comment에 대한 작업들

---
+++ app/src/main/java/com/example/eatssu/Comment.java
@@ -0,0 +1,26 @@
+package com.example.eatssu;
+
+public class Comment {
+    private String id;
+    private String comments;
+
+    Comment(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
+
+
--- app/src/main/java/com/example/eatssu/DetailBoardFragment.java
+++ app/src/main/java/com/example/eatssu/DetailBoardFragment.java
@@ -1,12 +1,31 @@
 package com.example.eatssu;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,9 +43,17 @@ public class DetailBoardFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DetailBoardFragment() {
-        // Required empty public constructor
-    }
+    private FirebaseFirestore db;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private RecyclerView RV;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private ArrayList<Comment> arrayList = new ArrayList<>();
+    //private ProgressDialog progressDialog;
+
+    public DetailBoardFragment() { }
 
     /**
      * Use this factory method to create a new instance of
@@ -59,6 +86,42 @@ public class DetailBoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("title"); // DB 테이블 연결 path:"Board"
+
+
+        db = FirebaseFirestore.getInstance();
         return inflater.inflate(R.layout.fragment_detail_board, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void EventChangeListener() {
+        db.collection("Board").orderBy("id", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        } else {
+                            for (DocumentChange dc : Objects.requireNonNull(value).getDocumentChanges()) {
+                                if (dc.getType() == DocumentChange.Type.ADDED) {
+                                    //ArrayList.add(dc.getDocument().toObject(Board.class));
+                                }
+                                adapter.notifyDataSetChanged();
+
+                            }
+                        }
+
+                    }
+                });
     }
 }
