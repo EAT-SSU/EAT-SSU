b6a9179ee8d53a7e089c91313882157e4b30f1a7
Yujin <jini1514@soongsil.ac.kr>
2022-12-13 23:00:44 +0900

    Revert "docs: 불필요한 문장 삭제"
    This reverts commit 7e5865c25136ebb6ac6dee27329849f2f21ef7ce.

--- Crawling/menu_dd_2.py
+++ Crawling/menu_dd_2.py
@@ -40,6 +40,7 @@ for i in ary:
     soup = BeautifulSoup(i.text, "html.parser")
     em=soup.find(text="중식4")
     도담식당메뉴3=em.find_next("b")
+    찐=도담식당메뉴3.find_next()
     print(도담식당메뉴3.text)
     ddlist3.append(도담식당메뉴3.text)
 
@@ -48,6 +49,7 @@ for i in ary:
     soup = BeautifulSoup(i.text, "html.parser")
     em=soup.find(text="석식1")
     도담식당석식=em.find_next("b")
+    찐석=도담식당석식.find_next()
     print(도담식당석식.text)
     ddlistN.append(도담식당석식.text)
 
